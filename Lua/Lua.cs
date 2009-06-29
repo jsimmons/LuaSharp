0723b7d13767a89ee8d6fae877d717f8f0f29646
Joshua Simmons <simmons.44@gmail.com>
2009-06-29 16:50:03 +1000

    Initial commit of class skeletons and project layout.

---
+++ Lua/Lua.cs
@@ -0,0 +1,42 @@
+// 
+// Lua.cs
+//  
+// Author:
+//       Joshua Simmons <simmons.44@gmail.com>
+// 
+// Copyright (c) 2009 Joshua Simmons
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+/// <summary>
+/// This class will form the basic P/Invoke API wrapper in order for applications to interface with Lua.
+/// This can be used as a seperate project, or its functionality can be used via the higher level abstraction provided by the LuaSharp class.
+/// </summary>
+
+using System;
+
+namespace Lua
+{
+	public class Lua
+	{		
+		public Lua()
+		{
+		}
+	}
+}
